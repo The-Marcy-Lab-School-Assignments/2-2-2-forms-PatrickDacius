836bb514f74ab92cf9f339271d6cff6a68fde237
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-05-18 00:00:30 -0400

    not finished

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
  *
  */
 
-const handleSubmit = () => {
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const formedData = new FormData(event.target)
+    const { objectConversion } = Object.fromEntries(formedData)
+
+    console.log('The final computed value', objectConversion)
 };
+
+document.getElementById('new-user-form').addEventListener('submit', handleSubmit)
